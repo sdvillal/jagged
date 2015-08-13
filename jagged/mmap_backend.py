989ec0530301fefb0575ab55bf3e8ca35a71a732
santi <sdvillal@gmail.com>
2015-08-13 15:27:54 +0200

    tidy mmap code

--- jagged/mmap_backend.py
+++ jagged/mmap_backend.py
@@ -13,6 +13,7 @@ except ImportError:
 
 
 class JaggedByMemMap(JaggedRawStore):
+    """Provides numpy arrays as views of an underlying memmapped array."""
 
     def __init__(self, path=None):
         super(JaggedByMemMap, self).__init__(path)
@@ -27,27 +28,42 @@ class JaggedByMemMap(JaggedRawStore):
         self._shape = None
         self._order = None
 
+    # --- Read
+
+    def _open_read(self):
+        self._read_meta()
+        if self._mm is None:
+            self._mm = np.memmap(self._path,
+                                 dtype=self._dtype, shape=self._shape, order=self._order,
+                                 mode='r')
+
+    def _get_hook(self, base, size, columns, address):
+        if address is None:
+            return self._mm[base:base+size]
+        address[:] = self._mm[base:base+size]
+        return address
+
+    # --- Write
+
+    def _open_write(self, data=None):
+        if not op.isfile(self._meta):
+            if data is None:
+                raise ValueError('data must not be None when bootstrapping storage')
+            self._dtype = data.dtype
+            self._order = 'F' if np.isfortran(data) else 'C'
+            self._shape = (0, data.shape[1])
+            self._write_meta()
+        self._read_meta()
+        self._mm = open(self._path, mode='a')
+
     def _append_hook(self, data):
         base = len(self)
         size = len(data)
         self._mm.write(str(data.data))
         self._shape = self._shape[0] + size, self._shape[1]
-        return base, size  # FIXME: take into account dtype size
+        return base, size
 
-    def write_meta(self):
-        """Writes the information about the array."""
-        with open(self._meta, 'wb') as writer:
-            pickle.dump((self._dtype, self._shape, self._order), writer, protocol=pickle.HIGHEST_PROTOCOL)
-
-    def read_meta(self):
-        """Reads the information about the array."""
-        if self._dtype is None:
-            if not op.isfile(self._meta):
-                raise Exception('Meta-information has not been stored yet')
-            with open(self._meta, 'rb') as reader:
-                self._dtype, self._shape, self._order = pickle.load(reader)
-        # TODO: growing length can be easily inferred from file size, no need to update _shape
-        #       what is best?
+    # --- Lifecycle
 
     @property
     def is_writing(self):
@@ -61,35 +77,28 @@ class JaggedByMemMap(JaggedRawStore):
     def is_open(self):
         return self._mm is not None
 
-    def _open_write(self, data=None):
-        if not op.isfile(self._meta):
-            if data is None:
-                raise ValueError('data must not be None when bootstrapping storage')
-            self._dtype = data.dtype
-            self._order = 'F' if np.isfortran(data) else 'C'
-            self._shape = (0, data.shape[1])
-            self.write_meta()
-        self.read_meta()
-        self._mm = open(self._path, mode='a')
-
-    def _open_read(self):
-        self.read_meta()
-        if self._mm is None:
-            self._mm = np.memmap(self._path,
-                                 dtype=self._dtype, shape=self._shape, order=self._order,
-                                 mode='r')
-
     def close(self):
         if self.is_writing:
             self._mm.close()
         self._mm = None
-        self.write_meta()
+        self._write_meta()
 
-    def _get_hook(self, base, size, columns, address):
-        if address is None:
-            return self._mm[base:base+size]
-        address[:] = self._mm[base:base+size]
-        return address
+    # --- Storage for underlying array shape, dtype, row/column order
+
+    def _write_meta(self):
+        """Writes the information about the array."""
+        with open(self._meta, 'wb') as writer:
+            pickle.dump((self._dtype, self._shape, self._order), writer, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def _read_meta(self):
+        """Reads the information about the array."""
+        if self._dtype is None:
+            if not op.isfile(self._meta):
+                raise Exception('Meta-information has not been stored yet')
+            with open(self._meta, 'rb') as reader:
+                self._dtype, self._shape, self._order = pickle.load(reader)
+
+    # --- Properties
 
     def _backend_attr_hook(self, attr):
         if attr == 'dtype':
@@ -97,3 +106,15 @@ class JaggedByMemMap(JaggedRawStore):
         if attr == 'shape':
             return self._shape
         raise ValueError('Unknown attribute %s' % attr)
+
+#
+# TODO: growing length can be easily inferred from file size, no need to update _shape
+#       what is best? Probably file size allows for more robust reentrancy...
+#
+# Remember that resize for numpy mmap objects never resize the file under the hood.
+# Fortunately we do not need it here, but was bitten by it when trying to be too clever
+#
+# Document that when using mode 'auto', everything returned is a view to the large
+# memmapped array. TEST that there is no much memory leaking going on. Document that
+# for this to work well we need 64bits, python >= 2.6
+#
