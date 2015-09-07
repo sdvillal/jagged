e88f06341763c891ba2f0d9241ed7e6522e2b9ad
santi <sdvillal@gmail.com>
2015-09-07 11:54:41 +0200

    context manager for blosc nthreads

--- jagged/compression/compressors.py
+++ jagged/compression/compressors.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import numpy as np
 
 from whatami import whatable
@@ -26,6 +27,15 @@ class Compressor(object):
 
 # --- Pimping Blosc to compress our arrays
 
+
+@contextmanager
+def _blosc_nthreads(n_threads):
+    old_nthreads = blosc.set_nthreads(n_threads)
+    yield old_nthreads
+    if old_nthreads != n_threads:
+        blosc.set_nthreads(old_nthreads)
+
+
 class BloscCompressor(Compressor):
 
     # This has quite an overhead beyond compression ATM
@@ -41,25 +51,25 @@ class BloscCompressor(Compressor):
         self._order = order
 
     def compress(self, x):
-        blosc.set_nthreads(self.n_threads)  # mmmm global, put in a context to reset
-        x = np.ascontiguousarray(x)  # LOOK AT THIS
-        shape, dtype, order = x.shape, x.dtype, ('F' if np.isfortran(x) else 'C')
-        if self._dtype is None:
-            self._shape, self._dtype, self._order = shape, dtype, order
-        else:
-            assert order == self._order
-            assert dtype == self._dtype
-            assert len(self._shape) == 1 or shape[1] == self._shape[1]
-        return blosc.compress_ptr(x.__array_interface__['data'][0],
-                                  x.size, x.dtype.itemsize,
-                                  shuffle=self.shuffle, cname=self.cname, clevel=self.level)
+        with _blosc_nthreads(self.n_threads):
+            x = np.ascontiguousarray(x)  # LOOK AT THIS
+            shape, dtype, order = x.shape, x.dtype, ('F' if np.isfortran(x) else 'C')
+            if self._dtype is None:
+                self._shape, self._dtype, self._order = shape, dtype, order
+            else:
+                assert order == self._order
+                assert dtype == self._dtype
+                assert len(self._shape) == 1 or shape[1] == self._shape[1]
+            return blosc.compress_ptr(x.__array_interface__['data'][0],
+                                      x.size, x.dtype.itemsize,
+                                      shuffle=self.shuffle, cname=self.cname, clevel=self.level)
 
     def decompress(self, cx):
-        blosc.set_nthreads(self.n_threads)  # mmmm global, put in a context to reset
-        x = blosc.decompress(cx)
-        x = np.frombuffer(x, dtype=self._dtype)  # beware gets an immutable array
-        if self._order == 'F':
-            np.asfortranarray(x)  # correct? makes copy and screwes up?
-        if len(self._shape) > 1:
-            x = x.reshape(-1, self._shape[1])
-        return x
+        with _blosc_nthreads(self.n_threads):
+            x = blosc.decompress(cx)
+            x = np.frombuffer(x, dtype=self._dtype)  # beware gets an immutable array
+            if self._order == 'F':
+                np.asfortranarray(x)  # correct? makes copy and screwes up?
+            if len(self._shape) > 1:
+                x = x.reshape(-1, self._shape[1])
+            return x
